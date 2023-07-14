5e81fb933d4d3e5e3e8326f3833dc47a6af276a8
Тарасов Антон Алексеевич <aatarasov@phoenixit.ru>
2023-07-14 21:46:06 +0300

    [1.0.12] fix please

--- beforeRelease.js
+++ beforeRelease.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const version = process.env.GITHUB_REF_NAME;
+const version = process.env.GITHUB_REF_NAME ?? '1.0.12';
 
 if (!version) {
     throw new Error('no version')
@@ -7,5 +7,8 @@ if (!version) {
 
 const packageJSON = JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf8'}));
 packageJSON.version = version;
-fs.writeFileSync('./package.json', JSON.stringify(packageJSON), { encoding: 'utf8', flag: 'w' })
-console.log('set new version', version)
+packageJSON.devDependencies = {};
+packageJSON.scripts = {};
+fs.writeFileSync('./dist/package.json', JSON.stringify(packageJSON), { encoding: 'utf8', flag: 'w' })
+fs.copyFileSync('./README.md', './dist/README.md');
+
